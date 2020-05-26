7e5e92e1ffea4a48bf7c492bee00ce82292f4f2f
margaritaLedo <margaritaledo@hotmail.com>
2020-05-26 20:03:16 +0200

    implement Interfaces for services. Closes #5

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,9 +1,10 @@
 package guru.springframework.sfgpetclinic;
 
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.runner.RunWith;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
